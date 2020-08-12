52f42da11601d4b8b9d40ac916e7f31a0045da34
Zorik Cherfas <zorik.cherfas@gmail.com>
2020-08-12 18:43:00 +0300

    Adding logger

--- server/main.py
+++ server/main.py
@@ -1,6 +1,20 @@
 from flask import Flask
 from flask import request, jsonify
+import logging
+
 app = Flask(__name__)
+logger = logging.getLogger("server")
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+fh = logging.FileHandler('server.log')
+fh.setLevel(logging.DEBUG)
+
+logger.addHandler(ch)
+logger.addHandler(fh)
+
 
 @app.route('/')
 def hello_world():
@@ -9,13 +23,14 @@ def hello_world():
 
 @app.route('/api/data_handler', methods = ['POST'])
 def receive_data():
-
+    logger.error('xx')
     if request.method == 'POST':
         data = request.args.get('data')
+        logger.debug(data)
         return jsonify(messsage='Success', statusCode=200)
 
 @app.route('/api/file_handler', methods = ['POST'])
-
+def receive_file():
     if request.method == 'POST':
         data = request.args.get('language')
         return jsonify(messsage='Success', statusCode=201)
